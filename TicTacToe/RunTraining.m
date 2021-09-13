ed7a9b5d5935b0b23d9351715466f8330cc090d4
aswyatt <adam.wyatt@stfc.ac.uk>
2021-09-13 09:19:10 +0100

    Still doesn't seem to train well

--- TicTacToe/RunTraining.m
+++ TicTacToe/RunTraining.m
@@ -4,9 +4,9 @@ rng(0);
 
 %	Create Agent Options
 agentOpts = rlQAgentOptions;
-agentOpts.EpsilonGreedyExploration.Epsilon = .1;
+agentOpts.EpsilonGreedyExploration.Epsilon = .3;
 agentOpts.EpsilonGreedyExploration.EpsilonDecay = 1e-3;
-agentOpts.EpsilonGreedyExploration.EpsilonMin = 1e-3;
+agentOpts.EpsilonGreedyExploration.EpsilonMin = eps(0);
 
 %	Set training options
 trainOpts = rlTrainingOptions;
@@ -18,8 +18,8 @@ trainOpts.Plots = "None";
 
 
 %	Train the agent
-obj = TrainingObj("NumAgents", 3, "LearnRate", .5, "AgentOpts", agentOpts);
-obj.Train(1000, trainOpts);
+obj = TrainingObj("NumAgents", 5, "LearnRate", .3, "AgentOpts", agentOpts);
+obj.Train(100, trainOpts);
 arrayfun(@(A) sum(cell2mat(getLearnableParameters(getCritic(A))), ...
 	"all", "omitnan"), obj.Agents)
 
@@ -27,7 +27,7 @@ arrayfun(@(A) sum(cell2mat(getLearnableParameters(getCritic(A))), ...
 
 T = TicTacToe;
 Na = length(obj.Agents);
-Ni = 1000;
+Ni = 100;
 P = zeros(Na, Na, 3);
 for ni=1:Ni
 	ind = randsample(1:Na, 2);
